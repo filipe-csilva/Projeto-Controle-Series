7ed1b8e8dbceffe26b39e0884c38624504fcaad1
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2026-06-07 17:55:04 -0300

    Criando view de episódios

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -1,12 +1,14 @@
 <x-layout title="Temporadas de {!! $series->nome !!}">
     <ul class="list-group">
         @foreach ($seasons as $season)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Temporada {{ $season->number }}
-                <span class="badge bg-secondary">
-                    {{ $season->episodes->count() }}
-                </span>
-            </li>                
+            <a href="{{ route('episodes.index', $season->id) }}">
+                <li class="list-group-item d-flex justify-content-between align-items-center">                
+                    Temporada {{ $season->number }}
+                    <span class="badge bg-secondary">
+                        {{ $season->episodes->count() }}
+                    </span>
+                </li>      
+            </a>          
         @endforeach
     </ul>
 </x-layout>
