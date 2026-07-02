230d3ebdd105e09cac1aca20533d730cfdc6d647
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2026-07-02 20:28:25 -0300

    Criando email automatico

---
+++ resources/views/mail/series-created.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+   # {{ $nomeSerie }} criada
+
+   A Série {{ $nomeSerie }} com {{ $qtdTemporadas }} temporadas e {{ $episodiosPorTemporada }} episódios.
+
+   Acesse aqui:
+
+   @component('mail::button', ['url' => route('seasons.index', $idSerie)])
+    Ver Série
+   @endcomponent
+@endcomponent
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\UsersController;
 use App\Http\Middleware\Autenticador;
+use App\Mail\SeriesCreated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -32,6 +33,11 @@ Route::get('/', function () {
     Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
     Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
     Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+    });
+
+    Route::get('/email', function ()
+    {
+        return new SeriesCreated('Série de teste', 2, 5, 10);
     });
 
     Route::get('/login', [LoginController::class, 'index'])->name('login');
