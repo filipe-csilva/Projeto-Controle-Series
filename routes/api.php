2fd84b0e6cc76c08c5ca4558835240f78b1e3e4a
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2026-08-02 23:08:34 -0300

    iniciando API no PHP

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use \App\Http\Controllers\Api\SeriesController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Suas rotas API aqui
+Route::prefix('v1')->group(function () {
+    Route::get('/status', function () {
+        return response()->json(['message' => 'API funcionando!']);
+    });
+    
+    Route::get('/series', [SeriesController::class, 'index']);
+});
